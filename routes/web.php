dfe61d76282b6ee0ff84e3b8596bc1652bb22291
dohaseif2 <dohaseif2@gmail.com>
2024-04-17 10:57:24 +0200

    using the Route::resource instead of methods

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts',[PostController::class,'index'])->name('posts.index');
+/*Route::get('/posts',[PostController::class,'index'])->name('posts.index');
 Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
 Route::post('/posts',[PostController::class,'store'])->name('posts.store');
 Route::get('/posts/{post}',[PostController::class,'show'])->where('post','[0-9]+')->name('posts.show');
 Route::get('/posts/{post}/edit',[PostController::class,'edit'])->where('post','[0-9]+')->name('posts.edit');
 Route::patch('posts/{post}',[PostController::class,'update'])->where('post','[0-9]+')->name('posts.update');
-Route::delete('posts/{post}',[PostController::class,'destroy'])->where('post','[0-9]+')->name('posts.destroy');
+Route::delete('posts/{post}',[PostController::class,'destroy'])->where('post','[0-9]+')->name('posts.destroy');*/
+
+Route::resource('posts',PostController::class);
